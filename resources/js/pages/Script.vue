a759a2b8385bc575fdd03dc3e4f762d61c718d62
vasylmatviishyn <vasyl@integratehq.com>
2025-05-31 14:22:28 +0300

    Initial commit

---
+++ resources/js/pages/Script.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <div>
+        test
+    </div>
+</template>
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/script', function () {
+    return Inertia::render('Script');
+})->name('test');
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
