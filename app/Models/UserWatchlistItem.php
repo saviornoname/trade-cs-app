946aced42e30f7957db32e9bcb5ad7053f9a8367
saviornoname <39801012+saviornoname@users.noreply.github.com>
2025-06-09 21:34:22 +0300

    setup test

--- app/Models/UserWatchlistItem.php
+++ app/Models/UserWatchlistItem.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\User;
 class UserWatchlistItem extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'title',
         'max_price_usd',
